9818dd48166577dd605135e68b58a4f9f448cab1
Michal Kuklis <michal.kuklis@gmail.com>
2014-01-13 10:49:25 -0500

    setup http server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var http = require('http');
 var markets = require('./markets');
 var port = process.env.PORT || 8080;
-var io = require('socket.io').listen(port);
+var server = http.createServer(function (req, res) { 
+	res.writeHead(200, { 'Content-Type': 'text/html' }); 
+	res.end('<p>Bitcoin Reader running</p>');
+});
+
+var io = require('socket.io').listen(server);
+
+server.listen(port);
 
 markets.on('data', function (data) {
 	console.log(data);
